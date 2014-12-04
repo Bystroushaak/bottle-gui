d463f331eca2cc5f15b7d4d4feaec9bf425fdeb7
Bystroushaak <bystrousak@kitakitsune.org>
2014-12-04 11:35:57 +0100

    list_routes() now returns list, group_routes() rewritten.

--- bottle_gui2.py
+++ bottle_gui2.py
@@ -34,15 +34,16 @@ DESCR_TEMPLATE = read_template("descr.html")
 
 
 # Classes =====================================================================
-class PathID(object):
-    def __init__(self, http_type, path):
-        self.http_type = http_type
-        self.path = path
+# class PathID(object):
+#     def __init__(self, method, path):
+#         self.method = method
+#         self.path = path
 
 
 class PathInfo(object):
-    def __init__(self, http_type, args, docstring, mdocstring, module_name):
-        self.http_type = http_type
+    def __init__(self, method, path, args, docstring, mdocstring, module_name):
+        self.method = method
+        self.path = path
         self.args = args
         self.docstring = self.sanitize(docstring)
         self.mdocstring = self.sanitize(mdocstring)
@@ -52,39 +53,89 @@ class PathInfo(object):
         if s:
             return s.replace("<", "&lt;").replace(">", "&gt;")
 
+    def __str__(self):  # TODO: remove
+        return self.method + " " + self.path
+
+
+class PathGroup(object):
+    def __init__(self, routes=[]):
+        self.routes = routes
+
+    def add_route(self, route):
+        self.routes.append(route)
+
+    def __str__(self):  # TODO: remove
+        return "group: " + " ".join(map(lambda x: str(x), self.routes)) + "\n"
+
 
 # Functions ===================================================================
 def list_routes():  # TODO: rename
     """
     Get dict
     """
-    return dict(map(  # create dict from rules
-        lambda r: (
-            PathID(r.method, r.rule.split("<")[0]),
-            PathInfo(
-                http_type=r.method,
-                args=r.get_callback_args(),
-                docstring=inspect.getdoc(r.get_undecorated_callback()) or "",
-                mdocstring=inspect.getdoc(
-                    inspect.getmodule(r.get_undecorated_callback())
-                ),
-                module_name=r.get_undecorated_callback().__module__
-            )
+    return map(
+        lambda r: PathInfo(
+            method=r.method,
+            path=r.rule.split("<")[0],
+            args=r.get_callback_args(),
+            docstring=inspect.getdoc(r.get_undecorated_callback()) or "",
+            mdocstring=inspect.getdoc(
+                inspect.getmodule(r.get_undecorated_callback())
+            ),
+            module_name=r.get_undecorated_callback().__module__
         ),
         bottle.default_app().routes
-    ))
+    )
 
 
+def group_routes(ungrouped_routes):
+    groups = []
 
+    # go from longest routes to shorter
+    routes = sorted(
+        ungrouped_routes,
+        key=lambda x: len(x.path),
+        reverse=True
+    )
 
+    # group and remove / routes - they would break grouping algorithm later
+    root_paths = filter(lambda x: x.path == "/", routes)
+    if root_paths:
+        groups.append(
+            PathGroup(root_paths)
+        )
 
+        # remove / routes if present
+        map(lambda x: routes.pop(), root_paths)
 
+    processed = set()
+    for route in routes:
+        # skip already processed routes
+        if route in processed:
+            continue
 
+        # find all routes, which starts with `route`
+        same_group = filter(
+            lambda x: x.path.startswith(route.path),
+            routes
+        )
 
+        # skip routes without groups for later processing
+        if len(same_group) <= 1:  # always at least one route
+            continue
 
+        groups.append(
+            PathGroup(same_group)
+        )
+        processed.update(same_group)
 
+    # don't forget to uprocessed routes
+    for route in set(routes) - processed:
+        groups.append(
+            PathGroup([route])
+        )
 
-
+    return groups
 
 
 
@@ -127,14 +178,21 @@ def main():
     )
 
 
-@route("/something")
+@route("/sources/hist")
 def test(something, something_else):
     """
     Here is docstring and so on.
     """
     pass
 
-@route("/something/else")
+@route("/sources/hist/xe")
+def test(something, something_else):
+    """
+    Here is docstring and so on.
+    """
+    pass
+
+@route("/sources/xex")
 def test2(something, something_else):
     """
     Another docstring
@@ -144,4 +202,9 @@ def test2(something, something_else):
 
 # Main program ================================================================
 if __name__ == '__main__':
-    main()
+    # main()
+    print "routes"
+    print map(lambda x: str(x), list_routes())
+    print
+    print "grouped"
+    print "".join(map(lambda x: str(x), group_routes(list_routes())))
