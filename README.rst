430cdb3c526ae2f9286c60dbc66be81a380cb300
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-02 17:02:22 +0100

    Added PIP package metadata files.

---
+++ README.rst
@@ -0,0 +1,18 @@
+Introduction
+============
+
+.. image:: https://badge.fury.io/py/bottle-gui.png
+    :target: http://badge.fury.io/py/bottle-gui
+
+.. image:: https://pypip.in/d/bottle-gui/badge.png
+        :target: https://crate.io/packages/bottle-gui?version=latest
+
+This package is used to vizualize services in `Bottle <http://bottlepy.org>`_
+web framework.
+
+Documentation
+-------------
+
+Full module documentation and description can be found at Read the Docs:
+
+- http://bottle-gui.readthedocs.org/
---
+++ setup.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from setuptools import setup, find_packages
+
+from docs import getVersion
+
+
+# Variables ===================================================================
+changelog = open('CHANGES.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    changelog
+])
+
+
+# Functions & classes =========================================================
+setup(
+    name='bottle-gui',
+    version=getVersion(changelog),
+    description="Package used to vizualize services in Bottle web framework.",
+    long_description=long_description,
+    url='https://github.com/Bystroushaak/bottle-gui',
+
+    author='Bystroushaak',
+    author_email='bystrousak@kitakitsune.org',
+
+    classifiers=[
+        "Framework :: Bottle",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license='MIT',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        "bottle",
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+            "requests"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
+)
